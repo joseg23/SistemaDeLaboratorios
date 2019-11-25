0da8b80659549d5897e8de44bad1f1356e776e16
00091318 <00091318@uca.edu.sv>
2019-11-25 13:34:19 -0600

    probando otro login

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,4 +1,67 @@
-let carnet_field = document.querySelector("#name");
+var pgClient = require("pg");
+var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var connection = pgClient.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nodelogin"
+});
+
+var app = express();
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function(request, response) {
+  response.sendFile(path.join(__dirname + "/index.pug"));
+});
+
+app.post("/auth", function(request, response) {
+  var username = request.body.username;
+  var password = request.body.password;
+  if (username && password) {
+    connection.query(
+      "SELECT * FROM accounts WHERE username = ? AND password = ?",
+      [username, password],
+      function(error, results, fields) {
+        if (results.length > 0) {
+          request.session.loggedin = true;
+          request.session.username = username;
+          response.redirect("/home");
+        } else {
+          response.send("Incorrect Username and/or Password!");
+        }
+        response.end();
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+    response.end();
+  }
+});
+
+app.get("/home", function(request, response) {
+  if (request.session.loggedin) {
+    response.send("Welcome back, " + request.session.username + "!");
+  } else {
+    response.send("Please login to view this page!");
+  }
+  response.end();
+});
+
+app.listen(3000);
+
+/*let carnet_field = document.querySelector("#name");
 let password_field = document.querySelector("#password");
 let submit_btn = document.querySelector("#submit_btn");
 let link_btn = document.querySelector("#link");
@@ -55,3 +118,4 @@ window.onload = function() {
     console.log("no papuh");
   }
 };
+*/
