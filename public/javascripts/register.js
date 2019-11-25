13dac5e291c56b95b56adf016d24f1721fde2bac
00091318 <00091318@uca.edu.sv>
2019-11-25 13:51:04 -0600

    new register

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,4 +1,50 @@
-let carnet_field = document.querySelector("#name");
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var db = require("pg");
+db.on("error", console.log.bind(console, "connection error"));
+db.once("open", function(callback) {
+  console.log("connection succeeded");
+});
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.post("/sign_up", function(req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var pass = req.body.password;
+
+  var data = {
+    name: name,
+    email: email,
+    password: pass
+  };
+  db.collection("details").insertOne(data, function(err, collection) {
+    if (err) throw err;
+    console.log("Record inserted Successfully");
+  });
+});
+
+app
+  .get("/", function(req, res) {
+    res.set({
+      "Access-control-Allow-Origin": "*"
+    });
+    return res.redirect("index.pug");
+  })
+  .listen(3000);
+
+console.log("server listening at port 3000");
+
+/*let carnet_field = document.querySelector("#name");
 let password_field = document.querySelector("#password");
 let submit_btn = document.querySelector("#submit_btn");
 let link_btn = document.querySelector("#link");
@@ -47,3 +93,4 @@ window.addEventListener("keyup", event => {
   } else {
   }
 });
+*/
